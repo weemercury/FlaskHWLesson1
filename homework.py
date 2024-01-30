70df4b051eee42ecfbf8fa80edcb55e11dd09bb6
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-01-30 15:39:18 +0300

    third commit

--- homework.py
+++ homework.py
@@ -1,36 +1,36 @@
-# from flask import Flask         
-# from flask import render_template
+from flask import Flask         
+from flask import render_template
 
 
-# app = Flask(__name__)           
+app = Flask(__name__)           
                                 
 
 
-# @app.route('/')                 
-# def hello_world():
-#     return render_template('base.html')
+@app.route('/')                 
+def hello_world():
+    return render_template('base.html')
 
 
-# @app.route('/clothes/')
-# def clothes():
-#     context = {'title': 'Одежда'}
-#     return render_template('clothes.html', **context)
+@app.route('/clothes/')
+def clothes():
+    context = {'title': 'Одежда'}
+    return render_template('clothes.html', **context)
 
 
-# @app.route('/shoes/')
-# def shoes():
-#     context = {'title': 'Обувь'}
-#     return render_template('shoes.html', **context)
+@app.route('/shoes/')
+def shoes():
+    context = {'title': 'Обувь'}
+    return render_template('shoes.html', **context)
 
 
-# @app.route('/jacket/')
-# def jacket():
-#     context = {'title': 'Куртка'}
-#     return render_template('jacket.html', **context)
+@app.route('/jacket/')
+def jacket():
+    context = {'title': 'Куртка'}
+    return render_template('jacket.html', **context)
 
 
-# if __name__ == '__main__':      
-#     app.run()   
+if __name__ == '__main__':      
+    app.run()   
     
     
     
